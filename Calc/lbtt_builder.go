e7bc2c1284b64f7990b5d92b1a2185044d89b34a
Jack Anderson <jack.anderson@bjss.com>
2024-10-18 10:26:22 +0100

    Add factory for creation of calculators by dates

--- Calc/lbtt_builder.go
+++ Calc/lbtt_builder.go
@@ -73,15 +73,3 @@ func (lbtt *LBTT) Build(err *error) *LBTT {
 	return lbtt
 
 }
-
-func MakeApr2021LBTT() *LBTT {
-	var err error
-	apr2021LBTT := NewLBTT().
-		WithBand(0, 145000, 145000, 0).
-		WithBand(145001, 250000, 105000, 0.02).
-		WithBand(250001, 325000, 75000, 0.05).
-		WithBand(325001, 750000, 425000, 0.1).
-		WithFinalBand(750000, 125000, 0.12).
-		Build(&err)
-	return apr2021LBTT
-}
